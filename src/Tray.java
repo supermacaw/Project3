e8801bb13cba7fff55664b00036d2199664a9cad
WiselyYang <wyang429@gmail.com>
2012-08-01 14:59:02 -0700

    Updated files via David's Tray class changes.

--- src/Tray.java
+++ src/Tray.java
@@ -7,7 +7,10 @@ public class Tray {
 	protected int lengthOfTray;
 	protected int widthOfTray;
     protected Block [][] config;
-    protected ArrayList<Block> blocksOnTray = new ArrayList<Block>(); //arraylist of blocks for ease of access when generating hash code
+    protected Block [][] goalConfig;
+    //protected ArrayList<Block> blocksOnTray = new ArrayList<Block>();
+    protected HashSet<Block> blocksOnTray = new HashSet<Block>();
+	protected ArrayList<Block> goalBlocks;
 	
 	public Tray(int rows, int cols) {
 		this.lengthOfTray = rows;
@@ -15,7 +18,7 @@ public class Tray {
         this.config = new Block[rows][cols];
 	}
 	
-	public void put (Block blockToAdd, int row, int col) {
+	public void place (Block blockToAdd, int row, int col) {
 		blockToAdd.upLCrow = row;
 		blockToAdd.upLCcol = col;
 		blocksOnTray.add(blockToAdd);
@@ -30,6 +33,7 @@ public class Tray {
 	}
 	
 	public void move (Block blockToMove, int row, int col) {
+		blocksOnTray.remove(blockToMove);
 		for (int m = blockToMove.upLCrow; m < blockToMove.upLCrow+blockToMove.length; m++){
 			for(int n = blockToMove.upLCcol; n < blockToMove.upLCcol+blockToMove.width; n++){
 				this.config[m][n] = null;
@@ -45,20 +49,75 @@ public class Tray {
 				this.config[i][j] = blockToMove;
 			}
 		}
+		blocksOnTray.add(blockToMove);
+	}
+	public void putGoalConfig(Block blockToAdd){
+		goalBlocks.add(blockToAdd);
+	}
+	
+	//check last moved block?... i feel like there's a better way of doing this
+	public boolean isAtGoal(){
+		for(int i = 0; i<goalBlocks.size(); i++){
+			if(!blocksOnTray.contains(goalBlocks.get(i))){//may have to check this contains method.. not sure if == or equals
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private boolean isOK() {
+		HashMap<Block, Integer> counts = new HashMap<Block, Integer>();
+		for (int m = 0; m < this.lengthOfTray; m++) {
+			for (int n = 0; n < this.widthOfTray; n++) {
+				Block curBlock = this.config[m][n];
+				if(curBlock!=null){
+					if(counts.containsKey(curBlock)){
+						counts.put(curBlock, counts.get(curBlock)+1);
+					}else{
+						counts.put(curBlock, 1);
+					}
+				}
+			}
+		}
+		Iterator<Block> i = blocksOnTray.iterator();
+		while(i.hasNext()){
+			Block b = i.next();
+			int numSpaces = b.length * b.width;
+			if(numSpaces != counts.get(b).intValue()) {
+				return false;
+			}
+		}
+		/*for(Block b : blocksOnTray){
+			int numSpaces = b.length * b.width;
+			if(numSpaces != counts.get(b).intValue()){
+				return false;
+			}
+		}*/
+		return true;
 	}
 	
 	public int hashCode(){
-		
+		int hashCode = 1;
+		Iterator<Block> i = blocksOnTray.iterator();
+		while (i.hasNext()) {
+		      Block x = i.next();
+		      hashCode = 31*hashCode + (x==null ? 0 : x.hashCode());
+		}
+		return hashCode;
 	}
+	
+	public boolean equals(Tray otherTray){
+		//compare length/width of tray? not needed apparently
+		for(int i = 0; i < this.lengthOfTray; i++){
+			for(int j = 0; j < this.widthOfTray; j++){
+				if(!otherTray.config[i][j].equals(this.config[i][j])){ //.equals or ==?
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
 
-
-/* This proves that boolean[][] is default to false
-    public static void main(String[] args) {
-        Tray test = new Tray(5, 5);
-        for (boolean[] b : test.isUsed) {
-            for (boolean bb : b) {
-                System.out.println(bb);
-            }
-        }
-    }*/
 }
+
