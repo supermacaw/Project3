01677d3b716e6e7bef13ce2cada834b2f68b3887
supermacaw <supermacaw@gmail.com>
2012-08-02 12:11:35 -0700

    temp

--- src/Tray.java
+++ src/Tray.java
@@ -135,77 +135,7 @@ public class Tray {
         return blocksOnTray;
     }
 	
-	 /**
-	  * Essentially a helper method for move, which is first called on by the solver for validity of a move on a block before the block is moved.
-	  * 
-	  * @param blockToMove
-	  * 		the block that is being attempted to move
-	  * @param endRow
-	  * 		the end row the upper left corner of the block would be moved to
-	  * @param endCol
-	  * 		the end col the upper left corner of the block would be moved to
-	  * @return
-	  */
-	public boolean isValidMove (Block blockToMove, int endRow, int endCol) {
-		if (endRow < 0 || endRow+blockToMove.length > this.lengthOfTray || endCol < 0 || endCol+blockToMove.width > this.widthOfTray) { //move would be out of bounds
-			return false;
-		}
-		if (blockToMove.upLCrow != endRow && blockToMove.upLCcol != endCol) { //cannot move diagonally
-			return false;
-		}
-		int dir = 0;
-		if (blockToMove.upLCrow - endRow > 0) {
-			dir = 1; //up
-		}
-		if (blockToMove.upLCrow - endRow < 0) {
-			dir = 2; //down
-		}
-		if (blockToMove.upLCcol - endCol > 0) {
-			dir = 3; //left
-		}
-		if (blockToMove.upLCcol - endCol < 0) {
-			dir = 4; //right
-		}
-		
-		switch (dir){
-		case 1: //up: check to see that the path is clear from current row to higher (lesser in index) row by making sure width of blockToMove is never blocked along the path
-			for(int i = blockToMove.upLCrow-1; i >= endRow; i--){
-				for (int j = blockToMove.upLCcol; j < blockToMove.upLCcol + blockToMove.width; j++){
-					if(this.config[i][j] != null){
-						return false;
-					}
-				}
-			}
-			
-		case 2: //down: check to see that the path is clear from current row to lower (higher in index) row by making sure width of blockToMove is never blocked along the path
-			for(int i = blockToMove.upLCrow+blockToMove.length; i <= endRow; i++){
-				for (int j = blockToMove.upLCcol; j < blockToMove.upLCcol + blockToMove.width; j++){
-					if(this.config[i][j] != null){
-						return false;
-					}
-				}
-			}
-			
-		case 3: //left: check to see that the path is clear from current col to more left (lesser in index) col by making sure length of blockToMove is never blocked along the path
-			for(int i = blockToMove.upLCcol-1; i >= endCol; i--){
-				for (int j = blockToMove.upLCrow; j < blockToMove.upLCrow + blockToMove.length; j++){
-					if(this.config[i][j] != null){
-						return false;
-					}
-				}
-			}
-			
-		case 4: //right: check to see that the path is clear from current col to more right (higher in index) col by making sure length of blockToMove is never blocked along the path
-			for(int i = blockToMove.upLCcol+blockToMove.width; i <= endCol; i++){
-				for (int j = blockToMove.upLCrow; j < blockToMove.upLCrow + blockToMove.length; j++){
-					if(this.config[i][j] != null){
-						return false;
-					}
-				}
-			}
-		}
-		return true;
-	}
+	 
 	
 	//check last moved block?... i feel like there's a better way of doing this
 	public boolean isAtGoal(ArrayList<Block> goalBlocks){
